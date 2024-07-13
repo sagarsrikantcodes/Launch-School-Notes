a7f6bd3058d0b66aae1ea2195d567d49a61b6c59
Sagar Suresh Srikant <sgrsuresh57@gmail.com>
2024-07-13 13:11:40 -0600

    Adding handlebars module to the calculator application

--- JS175_Networked_Applications_With_JavaScript/calculator/app.js
+++ JS175_Networked_Applications_With_JavaScript/calculator/app.js
@@ -1,10 +1,10 @@
 const HTTP = require('http');
 const URL = require('url').URL;
 const PORT = 3000;
-let nextVal = 0;
+const HANDLEBARS = require('handlebars');
 
-const HTML_START = `
-<!DOCTYPE html>
+const SOURCE = `
+  <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -41,36 +41,64 @@ const HTML_START = `
     <article>
       <h1>Arithmetic Calculator</h1>
       <table>
-        <tbody>`;
-
-const HTML_BODY_END = `
-        </tbody>`;
-
-const HTML_END = `
+        <tbody>
+          <tr>
+            <th>Num1:</th>
+            <td>
+              <a href='/?num1={{num1Decrement}}&num2={{num2}}'>- 1</a>
+            </td>
+            <td>{{num1}}</td>
+            <td>
+              <a href='/?num1={{num1Increment}}&num2={{num2}}'>+ 1</a>
+            </td>
+          </tr> 
+          <tr>
+            <th>Num2:</th>
+            <td>
+              <a href='/?num1={{num1}}&num2={{num2Decrement}}'>- 1</a>
+            </td>
+            <td>{{num2}}</td>
+            <td>
+              <a href='/?num1={{num1}}&num2={{num2Increment}}'>+ 1</a>
+            </td>
+          </tr> 
+          <tr>
+            <th>Sum:</th>
+            <td colspan='3'>{{sum}}</td>
+          </tr>
+          <tr>
+            <th>Product:</th>
+            <td colspan='3'>{{product}}</td>
+          </tr>
+          <tr>
+            <th>Quotient:</th>
+            <td colspan='3'>{{quotient}}</td>
+          </tr>
+          <tr>
+            <th>Remainder:</th>
+            <td colspan='3'>{{remainder}}</td>
+          </tr>
+        </tbody>
       </table>
     </article>
   </body>
 </html>`;
 
-
+const CALCULATIONS_TEMPLATE = HANDLEBARS.compile(SOURCE);
 
 function getSum(num1, num2) {
-  nextVal += 1;
   return num1 + num2;
 }
 
 function getProduct(num1, num2) {
-  nextVal += 1;
   return num1 * num2;
 }
 
 function getDivisor(num1, num2) {
-  nextVal += 1;
   return (num1 / num2).toFixed(2);
 }
 
 function getRemainder(num1, num2) {
-  nextVal += 1;
   return num1 % num2;
 }
 
@@ -80,59 +108,24 @@ function getParams(path, host) {
   return params;
 }
 
-function getContent(params) {
-  let num1 = Number(params.get('num1'));
-  let num2 = Number(params.get('num2'));
-  let sum = getSum(num1, num2);
-  let product = getProduct(num1, num2);
-  let quotient = getDivisor(num1, num2);
-  let remainder = getRemainder(num1, num2);
-  let HTML_TABLE = `
-    <tr>
-      <th>Num1:</th>
-      <td>
-        <a href='/?num1=${num1 - 1}&num2=${num2}'>- 1</a>
-      </td>
-      <td>${num1}</td>
-      <td>
-        <a href='/?num1=${num1 + 1}&num2=${num2}'>+ 1</a>
-      </td>
-    </tr> 
-    <tr>
-      <th>Num2:</th>
-      <td>
-        <a href='/?num1=${num1}&num2=${num2 - 1}'>- 1</a>
-      </td>
-      <td>${num2}</td>
-      <td>
-        <a href='/?num1=${num1}&num2=${num2 + 1}'>+ 1</a>
-      </td>
-    </tr> 
-    <tr>
-      <th>Sum:</th>
-      <td colspan='3'>${sum}</td>
-    </tr>
-    <tr>
-      <th>Product:</th>
-      <td colspan='3'>${product}</td>
-    </tr>
-    <tr>
-      <th>Quotient:</th>
-      <td colspan='3'>${quotient}</td>
-    </tr>
-    <tr>
-      <th>Remainder:</th>
-      <td colspan='3'>${remainder}</td>
-    </tr>`;
-  let HTML_TABLE_FOOTER = `
-    <tfoot>
-    <tr>
-      <th>Total number of computations:</th>
-      <td colspan='3'>${nextVal}</td>
-    </tr>
-  </tfoot>`;
+function render(template, data) {
+  let html = template(data);
+  return html;
+}
 
-  return `${HTML_START}${HTML_TABLE}${HTML_BODY_END}${HTML_TABLE_FOOTER}${HTML_END}`;
+function getComputationalDataObject(params) {
+  let data = {};
+  data.num1 = Number(params.get('num1'));
+  data.num2 = Number(params.get('num2'));
+  data.sum = getSum(data.num1, data.num2);
+  data.product = getProduct(data.num1, data.num2);
+  data.quotient = getDivisor(data.num1, data.num2);
+  data.remainder = getRemainder(data.num1, data.num2);
+  data.num1Increment = data.num1 + 1;
+  data.num1Decrement = data.num1 - 1;
+  data.num2Increment = data.num2 + 1;
+  data.num2Decrement = data.num2 - 1;
+  return data;
 }
 
 const SERVER = HTTP.createServer((req, res) => {
@@ -144,7 +137,8 @@ const SERVER = HTTP.createServer((req, res) => {
     res.end();
   } else {
     let params = getParams(path, `http://localhost:${PORT}`);
-    let content = getContent(params);
+    let data = getComputationalDataObject(params);
+    let content = render(CALCULATIONS_TEMPLATE, data);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.write(`${content}\n`);
